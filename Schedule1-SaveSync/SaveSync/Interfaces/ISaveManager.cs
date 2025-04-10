161b1967ca8ecbec5936377676b3de391726fe47
boukolos <joseph@boukolos.com>
2025-04-10 15:21:00 -0400

    Add simple, non final fields to ISaveManager

--- Schedule1-SaveSync/SaveSync/Interfaces/ISaveManager.cs
+++ Schedule1-SaveSync/SaveSync/Interfaces/ISaveManager.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 namespace Schedule1_SaveSync.SaveSync.Interfaces
 {
     internal interface ISaveManager
-    {
+    { // this is incredibly non final
+        public List<ISaveable> SaveGames { get; set; }
+        public bool LoadSave(ISaveable save);
+        public bool SaveGame(ISaveable save);
     }
 }
